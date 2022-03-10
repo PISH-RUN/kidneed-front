d84758835ba5888cfaea36a9ce72de7763cd9499
mohammad-ILA <mghazim93@gmail.com>
2022-03-10 16:11:26 +0330

    some changes

--- src/pages/earth.js
+++ src/pages/earth.js
@@ -5,6 +5,49 @@ import UserProfile from 'earth/components/userProfile'
 import Image from 'next/image'
 import SearchBox from 'earth/components/searchBox'
 import Card from '../earth/components/card';
+import ParentDashboardLayout from 'layouts/parent-dashboard-layout'
+import {
+  Typography,
+  Box,
+  Stack,
+  Grid,
+  Rating,
+  Avatar,
+  Paper,
+  Button,
+  IconButton,
+} from "@mui/material";
+import ArrowDown from "layouts/icons/arrow-down";
+
+const SideDashboard = () => {
+  return (
+    <>
+      <Stack
+        direction="row"
+        alignItems="center"
+        spacing={2}
+        sx={{ py: 2, pr: 3, cursor: "pointer" }}
+      >
+        <Avatar
+          sx={{ width: 90, height: 90, p: 2, background: "#E2F1FD" }}
+          src="/images/avatar-woman.png"
+        />
+        <Box flexGrow={1}>
+          <Typography variant="h5">حسنا خانوم</Typography>
+          <Typography variant="h6" sx={{ color: "#8CA3A5" }}>
+            2500 سکه
+          </Typography>
+        </Box>
+        <Box>
+          <ArrowDown sx={{ color: "#8CA3A5" }} />
+        </Box>
+      </Stack>
+      <Box>
+        <Box component="img" src="/images/pd-test.png" sx={{ px: 2 }} />
+      </Box>
+    </>
+  );
+};
 const Earth = () => {
   const { Step } = Steps;
   const [step, setStep] = useState(0);
@@ -43,5 +86,8 @@ const Earth = () => {
   );
 };
 
+Earth.getLayout = (children) => (
+  <ParentDashboardLayout SideComponent={<SideDashboard />}>{children}</ParentDashboardLayout>
+);
 export default Earth;
 Earth.guard = () => true
