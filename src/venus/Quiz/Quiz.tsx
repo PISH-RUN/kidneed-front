2af4f63ffd0bd4d47598e6885abf0510790ee749
sajjad jalali <msajjad.jm@gmail.com>
2022-03-10 19:03:03 +0330

    some changes

--- src/venus/Quiz/Quiz.tsx
+++ src/venus/Quiz/Quiz.tsx
@@ -8,14 +8,11 @@ import styles from "./Quiz.module.css";
 
 export const Quiz: React.FC<{way?: string, childId?: number}> = (props) => {
 
-    const [data, setData] = useState<any>()
+    const [data, setData] = useState<Array<any>>([])
 
     useEffect(() => {
-        strapi.setToken(
-            "d74834e7df03b4b298df6d5da3df2f3a82f37eba1563ce1bf9cf1c8e6c66caff0e0619843fe6c1eecf6aaae5152f4aa3ea09ba1fa8da715407661516c607f8e8fea5cd22fc124e41335e0fb86f7268e65930903b56c854653d7a4a2f8d237b63cf729da083064ed9eadc0755c6a3c6a6b03fa5c7e7cfb029f73b6e89f4be827e"
-        );
         strapi
-            .request<any>("get", "/venus/question/", {
+            .request<any>("get", `/venus/question/?category=${props.way}&child=${props.childId}`, {
                 data: {
                     data: {
                         category: props.way,
@@ -24,8 +21,7 @@ export const Quiz: React.FC<{way?: string, childId?: number}> = (props) => {
                 },
             })
             .then((response) => {
-                console.log(response.data)
-                setData(response.data);
+                setData(response);
             })
             .catch((error) => {
                 console.log(error);
@@ -41,14 +37,9 @@ export const Quiz: React.FC<{way?: string, childId?: number}> = (props) => {
             <Text style={{ fontSize: "16px" }}>لطفا موارد زیر را در مورد فرزندتان مشخص نمایید</Text>
             <Form layout="vertical" >
                 <div className={styles.questionsWrapper}>
-                    <QuestionSlider label="سلام" name="question" />
-                    <QuestionSlider label="سلام" name="question" />
-                    <QuestionSlider label="سلام" name="question" />
-                    <QuestionSlider label="سلام" name="question" />
-                    <QuestionSlider label="سلام" name="question" />
-                    <QuestionSlider label="سلام" name="question" />
-                    <QuestionSlider label="سلام" name="question" />
-                    <QuestionSlider label="سلام" name="question" />
+                    {data.map((item: any) => {
+                        return <QuestionSlider key={item.id} label={item.title} name={`a-${item.id}`} />
+                    })}
                 </div>
             </Form>
         </ContentWrapper>
