a8bf0990b8ca5098d01c4014223804cd613d44b3
Hamid Sarani <h.sarani75@gmail.com>
2022-04-06 16:03:15 +0430

    update video player

--- src/pages/players/video.tsx
+++ src/pages/players/video.tsx
@@ -1,59 +1,33 @@
-import React, { ReactElement, useCallback, useEffect, useRef, useState } from "react";
-import videojs from "video.js";
+import React, { ReactElement, useRef } from "react";
+// @ts-ignore
+import ReactPlayer from 'react-player'
 import { useRouter } from "next/router";
 import { Guard } from "@kidneed/types";
-import BaseLayout from "layouts/baseLayout";
-
-const PlayerCSS = () => {
-  return (
-    <>
-      <div>
-        <video
-          id="my-video"
-          className="video-js vjs-theme-city"
-          playsInline
-          controls
-          preload="auto"
-          data-setup='{ "techOrder": ["youtube"], "sources": [{ "type": "video/youtube", "src": "https://www.youtube.com/watch?v=IkOVe40Sy0U"}] }'
-        ></video>
-      </div>
-    </>
-  );
-};
+import BaseLayout from "../../layouts/baseLayout";
+import Activity from "./activity";
 
 const Video = () => {
   const { query } = useRouter();
   const { url, thumbnail } = query;
 
-  const [videoEl, setVideoEl] = useState(null);
-  const onVideo = useCallback((el) => {
-    setVideoEl(el);
-  }, []);
+  const time = useRef();
+  const state = {
+    video: {
+      src: url,
+      poster: thumbnail
+    }
+  };
 
-  useEffect(() => {
-    if (videoEl == null) return;
-    const player = videojs(videoEl, {
-      autoplay: false,
-      sources: [
-        {
-          src: url as string,
-          type: 'application/x-mpegURL'
-        }
-      ]
-    });
-    return () => {
-      player.dispose();
-    };
-  }, [videoEl]);
+  const onVideoTimeUpdate = (t: any) => {
+    time.current = t;
+  };
 
   return (
     <div className="tw-justify-center tw-items-center tw-flex tw-w-full tw-h-screen tw-bg-sky-100">
-      <video ref={onVideo} className="video-js" playsInline />
-      <PlayerCSS />
+      <ReactPlayer url={url} />
     </div>
   );
 };
-
 
 const guard: Guard = (matcher, ctx, router) => {
   if (matcher("guest")) {
