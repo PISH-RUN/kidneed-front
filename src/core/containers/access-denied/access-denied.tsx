9d6d28c770c6e29f149029b9df61d038972ee776
Hamid Sarani <h.sarani75@gmail.com>
2022-05-25 11:49:44 +0430

    fix access denied issue

--- src/core/containers/access-denied/access-denied.tsx
+++ src/core/containers/access-denied/access-denied.tsx
@@ -1,7 +1,7 @@
 export default function AccessDenied() {
   return (
     <div className="tw-h-screen tw-flex tw-justify-center tw-items-center">
-      دسترسی غیر مجاز | 403
+      لطفا صبر کنید...
     </div>
   );
 }
